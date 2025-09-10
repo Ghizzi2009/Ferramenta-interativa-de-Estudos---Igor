dca6c8178e0223d4419ae03faa629fd0fd04d800
Ghizzi2009 <igor.ghizzi@escola.pr.gov.br>
2025-09-10 13:36:18 +0000

    criamos o j7 da aula 7

---
+++ Perguntas.js
@@ -0,0 +1,45 @@
+criaCartao(
+    'Programação',
+    'O que é HTML',
+    'È uma linguagem de programação que nos permite criar a estrutura do nosso site'
+)
+criaCartao(
+    'Programação',
+    'O que é CSS',
+    'È uma linguagem de programação utilizado para dar estilo a página'
+)
+criaCartao(
+    'Programação',
+    'O que é Javacript',
+    'O Javascript (JS) é uma linguagem de programação que nos permite interagir com a páina'
+)
+criaCartao(
+    'Programação',
+    'O que é a teg footer',
+    'è a tag utiliada para criar o rodapé'
+)
+criaCartao(
+    'Programação',
+    'Qual a dierença entre a teg title que usamos na teg head e a tag h1 que usamos na teg boody?',
+    'A tag title aparecerá na aba da página que sera aberta e a tag de titulo h1 é usado no corpo so site como título principal da página'
+)
+criaCartao(
+    'Programação',
+    'Qual a dierença entre as tegs head e header?',
+     'head é a teg de configurações, tudo que fizermos dentro dela, não aparecerá na página do nosso site, Header é tag de cabeçalho da página'
+)
+criaCartao(
+    'Programação',
+    'Pra que serve o :root utiliado no CSS',
+    'Serve para salvar variáveis para serem utiliadas durante o desenvolvimento da página, como por exemplo, cores e fontes'
+)
+criaCartao(
+    'Programação',
+    'Qual a dierença entre margin e padding',
+    'Margin é espaçamento externo enquanto o padding é espaçamento interno'
+)
+criaCartao(
+    'Programação',
+    'Para que serve a tag article',
+    'Para criar um conteudo ou elemento indepenente. No nosso caso estamos usando para criar os cartões de estudo.'
+)
