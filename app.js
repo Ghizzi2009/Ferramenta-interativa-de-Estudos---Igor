b11063f3bb43390ea933249bf8cadf0f48935945
Ghizzi2009 <igor.ghizzi@escola.pr.gov.br>
2025-09-24 13:35:47 +0000

    terminamos a aula 8

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 function criaCartao (conteudo, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    container.appendChild(cartao)
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
+`
+
 }
+
